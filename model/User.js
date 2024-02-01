4795024951c16a8be6859e891df43a29ccdab9cf
mohamednasser11 <mohamednaser1000.mn@gmail.com>
2024-02-01 21:46:51 +0200

    removing timestamp

--- model/User.js
+++ model/User.js
@@ -1,23 +1,20 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-      min: 6,
-    },
-    password: {
-      type: String,
-      required: true,
-      min: 8,
-    },
-    email: {
-      type: String,
-      required: true,
-    },
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    min: 6,
   },
-  { timestamps: true }
-);
+  password: {
+    type: String,
+    required: true,
+    min: 8,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+});
 
 module.exports = mongoose.model("User", userSchema);
